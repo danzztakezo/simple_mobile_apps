19ca83eeda54e722a07a86df659c09c9975f612b
dani himawan <danzztakezo@gmail.com>
2017-06-14 21:54:12 +0700

    feat(design) add page activation

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SignupPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { ActivationUserPage } from '../activation-user/activation-user';
+
 @IonicPage()
 @Component({
   selector: 'page-signup',
@@ -15,6 +11,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SignupPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  signup_proccess(){
+    this.navCtrl.push(ActivationUserPage);
   }
 
   ionViewDidLoad() {
